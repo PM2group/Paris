d18277c685c5648e8d0b75fb569b35eaf0943916
xatayuu <45218547+xatayuu@users.noreply.github.com>
2018-12-05 10:38:02 +0900

    Add files via upload

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  
+  get 'search/member'
+  get 'search/company'
+  get 'search/chat'
+
+  
+  get '/login_com', to: 'sessions_com#new'
+  post '/login_com', to: 'sessions_com#create'
+  delete '/logout_com', to: 'sessions_com#destroy'
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  root to: 'tops#index'
+  resources :users
+  resources :user_pages
+  resources :companys
+  resources :com_pages
+  resources :tops
+end
