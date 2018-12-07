119a32a04f01e645446e0f8999fbe00c5fad1574
xatayuu <xatayuu@yahoo.co.jp>
2018-12-07 10:44:42 +0900

    member 10:44

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,14 @@ class SearchController < ApplicationController
   skip_before_action :login_required
   skip_before_action :login_com_required
   def member
-    @member = User.all
     @s_word = params[:s_word]
     @e_word = params[:e_word]
     @des_occ = params[:des_occ]
     @des_loc = params[:des_loc]
+    @quali = params[:puali]
+    @univ = params[:univ]
+    @lan_exp = params[:lan_exp]
+    @member = User.where(des_occupation: @des_occ)
 
   end
 
