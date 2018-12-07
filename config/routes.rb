6bc00d5a16f07845f70baabd79346a0742464b04
xatayuu <xatayuu@yahoo.co.jp>
2018-12-07 15:40:39 +0900

    asa

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
-  
-<<<<<<< HEAD
-  get 'search/member'
-  get 'search/company'
-  get 'search/chat'
 
   get 'search/company/:id', to: 'user_pages#index'
   post 'search/company/:id', to: 'company_offers#create'
   
-=======
   get 'search/member' => 'search#member'
   get 'search/company' => 'search#company'
   get 'search/chat' => 'search#chat'
@@ -19,7 +13,6 @@ Rails.application.routes.draw do
 
   get 'search/member/:id' => "member"
 
->>>>>>> doraemon/master
   get '/login_com', to: 'sessions_com#new'
   post '/login_com', to: 'sessions_com#create'
   delete '/logout_com', to: 'sessions_com#destroy'
@@ -27,7 +20,6 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-<<<<<<< HEAD
 
   delete '/user_pages', to: 'company_offer#destroy'
   
@@ -37,14 +29,7 @@ Rails.application.routes.draw do
   resources :companys
   resources :com_pages
   resources :company_offers
-=======
-  root to: 'tops#index'
   resources :chat_pages
   resources :chat_states
-  resources :users
-  resources :user_pages
-  resources :companys
-  resources :com_pages
->>>>>>> doraemon/master
   resources :tops
 end
