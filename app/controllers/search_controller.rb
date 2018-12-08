66774fd7968983ed79b817edbdcadd5ace16a96f
xatayuu <xatayuu@yahoo.co.jp>
2018-12-08 15:34:48 +0900

    調整

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -44,13 +44,48 @@ class SearchController < ApplicationController
   end
 
   def company
-    @company
-    @s_word = params[:s_word]
-    @e_word = params[:e_word]
+
+    s_word = params[:s_word]
+    e_word = params[:e_word]
+    list = ["NOT user_name IS NULL"]
+
+    des_occ = params[:des_occ]
+    des_loc = params[:des_loc]
+    quali = params[:quali]
+    univ = params[:univ]
+    lan_exp = params[:lan_exp]
+
+    unless des_occ.empty? then
+      list[0].concat(" AND des_occupation = ?")
+      list.push(des_occ)
+    end
+
+    unless des_loc.empty? then
+      list[0].concat(" AND des_location = ?")
+      list.push(des_loc)
+    end
+    
+    unless quali.empty? then
+      list[0].concat(" AND qualification = ?")
+      list.push(quali)
+    end
+
+    unless univ.empty? then
+      list[0].concat(" AND univercity = ?")
+      list.push(univ)
+    end
+    
+    unless lan_exp.empty? then
+      list[0].concat(" AND lang_ex = ?")
+      list.push(lan_exp)
+    end
+
+    @member = User.order("updated_at").where(list)
+
   end
 
   def chat
     @chat = User.all
   end
-  
+
 end
