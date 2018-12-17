02dd1a16f5185d3c6f342578aa9da9e912609e27
hanachansan <hanachansan3107@gmail.com>
2018-12-17 09:22:10 +0900

    参加ボタン

--- app/controllers/chat_pages_controller.rb
+++ app/controllers/chat_pages_controller.rb
@@ -10,6 +10,7 @@ class ChatPagesController < ApplicationController
     @chat_page = ChatPage.find(params[:id])
     @chat_states = @chat_page.chat_states.all
     @chat_join_mems = @chat_page.chat_join_mems.all
+    @chat_join_mem =  @chat_page.chat_join_mems.new
     @chat_state = ChatState.new
     @report = Report.new
 
@@ -18,6 +19,40 @@ class ChatPagesController < ApplicationController
     elsif current_user
       @report_man = current_user.id
     end
+
+    /参加ボタンが押されたとき/ 
+    if params[:join_chat]
+      if @chat_page.join_mem < @chat_page.max_mem
+        @chat_page.join_mem += 1
+        if current_user
+          @chat_join_mem.mem_id = current_user.id
+          @chat_join_mem.mem_name = current_user.user_name    
+        elsif current_company
+          @chat_join_mem.mem_id = current_company.id
+          @chat_join_mem.mem_name = current_company.com_name
+        end        
+        if @chat_page.join_mem >= @chat_page.max_mem
+          @chat_page.rec = false
+        end
+        @chat_page.save
+        redirect_to chat_page_path(@chat_page), notice: "チャットに参加しました" and return
+      end
+    end
+
+    /新規参加者かを判定する/
+    @new_mem = true
+    @chat_join_mems.each do |chat_join_mem|
+      if current_user
+        if current_user.user_name == chat_join_mem.mem_name
+          @new_mem = false
+        end
+      elsif current_company
+        if current_company.com_name == chat_join_mem.mem_name
+          @new_mem = false
+        end
+      end
+    end
+
 
     session[:chat_page_id] = @chat_page.id
   end
@@ -31,17 +66,15 @@ class ChatPagesController < ApplicationController
     @chat_page = ChatPage.new(chat_page_params)
 
     if @chat_page.start_date.nil?
-      redirect_to new_chat_page_path, notice: "入力された項目に誤りがあります" and return
+      redirect_to new_chat_page_path, notice: "開催日時が入力されていません" and return
     end
-    puts 1213241
 
     if @chat_page.max_mem.nil?
-      redirect_to new_chat_page_path, notice: "入力された項目に誤りがあります" and return
+      redirect_to new_chat_page_path, notice: "最大人数が入力されていません" and return
     end 
-    puts 121241
     
     if @chat_page.theme.empty?
-      redirect_to new_chat_page_path, notice: "入力された項目に誤りがあります" and return
+      redirect_to new_chat_page_path, notice: "テーマが入力されていません" and return
     end 
 
 
@@ -77,7 +110,7 @@ class ChatPagesController < ApplicationController
 
     unless @chat_page.finish_date.nil?
       if @chat_page.start_date > @chat_page.finish_date
-          redirect_to new_chat_page_path, notice: "入力された項目に誤りがあります" and return
+          redirect_to new_chat_page_path, notice: "終了日時が開催日時より前になっています" and return
       end
     end
  
