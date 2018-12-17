ff6ba8f55d3cdc69dc70738539bac005f2e29302
Kurihashi Yuta <kuragetilyuusa0628@gmail.com>
2018-12-18 06:40:38 +0900

    アカウント削除によるオファー処理、マイページの表示項目の追加

--- app/controllers/super_users_controller.rb
+++ app/controllers/super_users_controller.rb
@@ -28,10 +28,18 @@ class SuperUsersController < ApplicationController
   def destroy
     if (params[:id].to_i / 1000000000)==1
       user = User.find(params[:id])
+      company_offers = CompanyOffer.where(mem_id: user.id)
+      company_offers.each do |offer|
+        offer.destroy
+      end
       user.destroy
       redirect_to super_users_url, notice: "アカウントID[#{params[:id]}]を削除しました"
     elsif (params[:id].to_i / 1000000000)==2
       com = Company.find(params[:id])
+      company_offers = CompanyOffer.where(com_id: com.id)
+      company_offers.each do |offer|
+        offer.destroy
+      end
       com.destroy
       redirect_to super_users_url, notice: "アカウントID[#{params[:id]}]を削除しました"
     end
