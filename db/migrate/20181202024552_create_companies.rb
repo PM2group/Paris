c2c8c45c9613c9d683af991d25a7ab740924553e
Kurihashi Yuta <kuragetilyuusa0628@gmail.com>
2018-12-23 11:53:28 +0900

    企業のアドレスにuniqueを追加

--- db/migrate/20181202024552_create_companies.rb
+++ db/migrate/20181202024552_create_companies.rb
@@ -4,7 +4,7 @@ class CreateCompanies < ActiveRecord::Migration[5.2]
       t.string :com_name,limit: 50,null: false
       t.string :occupations,limit: 100,null: false
       t.string :location,limit: 50,null: false
-      t.string :com_info, null: false,limit: 30
+      t.string :com_info, null: false,limit: 30,unique: true
       t.string :condition,null: false,limit: 100
       t.integer :salary, null: false
       t.string :password_digest
